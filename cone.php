6c90db7c2f3e23dad04a1544cf1220afecd4cdaa
danyerbert <danyerbert@gmail.com>
2023-12-21 11:55:38 -0400

    Correcion de conexion y realizacion de validaciones con respecto a la soluciones de los ticket

--- cone.php
+++ cone.php
@@ -1,5 +1,5 @@
 <?php 
 
-// $conn = mysqli_connect('bd.industriacanaima.gob.ve','intranet','123456','Intranet');
-$conn = mysqli_connect('localhost', 'root', '', 'bbs1');
+ $conn = mysqli_connect('bd.industriacanaima.gob.ve','intranet','123456','Intranet');
+//$conn = mysqli_connect('localhost', 'root', '', 'bbs1');
 ?>
