8b05aea651f1623f27d7100a4f4332952731a52b
danyerbert <danyerbert@gmail.com>
2024-05-06 13:12:17 -0400

    Correcion de conexion

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-   // $conn = mysqli_connect('bd.industriacanaima.gob.ve','intranet','123456','Intranet');
-    $conn = mysqli_connect('localhost', 'root', '','intranet');
+   $conn = mysqli_connect('bd.industriacanaima.gob.ve','intranet','123456','Intranet');
+    // $conn = mysqli_connect('localhost', 'root', '','intranet');
 
 ?>
