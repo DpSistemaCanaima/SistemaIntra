8cfccce1252142bea9338016e2d0e5dbd2d4ee52
morenonavarroluis <morenoluismanu@gmail.com>
2024-06-17 02:05:39 -0400

    linea de tiempo

--- modal/modal_tiempo.php
+++ modal/modal_tiempo.php
@@ -26,113 +26,104 @@
             <div class="c">
                 <div class="date">2004</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+             <p>   •	Se alcanza la producción de 100.000 computadoras Canaima.</p>
+             <p>   •	Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+
                 </div>
             </div>
             <div class="d">
                 <div class="date">2005</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+              <p> •	Se lanza la segunda generación de computadoras Canaima, con mayor potencia y nuevas funcionalidades.</p>            
+              <p>     •	Se comienza a exportar computadoras Canaima a otros países de América Latina.</p>
+
                 </div>
             </div>
             <div class="f">
                 <div class="date">2006</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                <p>•	Se alcanza la producción de 500.000 computadoras Canaima.</p>
+                    <p>•	Se abre una nueva planta de ensamblaje en el estado Bolívar.</p>
                 </div>
             </div>
             <div class="g">
                 <div class="date">2007</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                  
+                    <p>•	Se lanza la tercera generación de computadoras Canaima, con aún más potencia y nuevas funcionalidades.
+
+</p>
+                 <p>•	Se comienza a distribuir computadoras Canaima a estudiantes universitarios.</p>  
                 </div>
             </div>
             <div class="h">
                 <div class="date">2008</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se alcanza la producción de 1.000.000 computadoras Canaima.</p>
+                   <p>•	Se abre una nueva planta de ensamblaje en el estado Anzoátegui.</p>
                 </div>
             </div>
             <div class="j">
                 <div class="date">2009</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se lanza la cuarta generación de computadoras Canaima, con pantallas táctiles y otras características innovadoras.</p>
+                   <p>•	Se comienza a distribuir computadoras Canaima a comunidades indígenas.</p>
                 </div>
             </div>
             <div class="k">
                 <div class="date">2010</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                  <p>•	Se alcanza la producción de 2.000.000 computadoras Canaima.</p>
+                  <p>•	Se abre una nueva planta de ensamblaje en el estado Apure.</p>
                 </div>
             </div>
             <div class="l">
                 <div class="date">2011</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se lanza la quinta generación de computadoras Canaima, con aún más funciones y mayor rendimiento.</p>
+                   <p>•	Se comienza a distribuir computadoras Canaima a personas con discapacidad.</p>
                 </div>
             </div>
             <div class="q">
                 <div class="date">2012</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se alcanza la producción de 3.000.000 computadoras Canaima.</p>
+                   <p>•	Se abre una nueva planta de ensamblaje en el estado Amazonas.</p>
                 </div>
             </div>
             <div class="w">
                 <div class="date">2013</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                    <p>•	Se lanza la sexta generación de computadoras Canaima, con procesadores más potentes y nuevas aplicaciones educativas.</p>
+                    <p•	Se comienza a distribuir computadoras Canaima a instituciones públicas.></p>
                 </div>
             </div>
             <div class="e">
                 <div class="date">2014</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se alcanza la producción de 4.000.000 computadoras Canaima.</p>
+                   <p>•	Se abre una nueva planta de ensamblaje en el estado Delta Amacuro.</p>
                 </div>
             </div>
             <div class="r">
                 <div class="date">2015</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se lanza la séptima generación de computadoras Canaima, con diseños más ergonómicos y nuevas funcionalidades.</p>
+                   <p>•	Se comienza a distribuir computadoras Canaima a emprendedores y pequeñas empresas.</p>
                 </div>
             </div>
             <div class="t">
                 <div class="date">2016</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                   <p>•	Se alcanza la producción de 5.000.000 computadoras Canaima.</p>
+                   <p>•	Se abre una nueva planta de ensamblaje en el estado Monagas.</p>
                 </div>
             </div>
             <div class="n">
                 <div class="date">2017</div>
                 <div class="content">
-                    <h2>Producción de 100.000 Computadoras Canaima</h2>
-                    <p>Se alcanza la producción de 100.000 computadoras Canaima.</p>
-                    <p>Se abre una nueva planta de ensamblaje en el estado Carabobo.</p>
+                  <p>•	Se lanza la octava generación de computadoras Canaima, con tecnología de punta y mayor conectividad.</p>
+                  <p>•	Se comienza a distribuir computadoras Canaima a hogares de bajos recursos.</p>
                 </div>
             </div>
         </div>
