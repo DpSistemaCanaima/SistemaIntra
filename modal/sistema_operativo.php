d123087ba0a197cce7b65f62e0866ca387fc338a
navarroluismanu <morenoluismanu@gmail.com>
2024-07-03 17:04:45 -0400

    cambios

--- modal/sistema_operativo.php
+++ modal/sistema_operativo.php
@@ -4,8 +4,9 @@
 
 
 <a href="#close" title="Close" class="close">X</a>
-Conoce el sistema instalado en nuestras Canaimitas
+
 <h2 class="titulo">
+<p>Conoce el sistema instalado en nuestras Canaimitas</p>
 Canaima GNU Linux
 </h2>
 
@@ -14,7 +15,7 @@ Canaima GNU Linux
        <div class="contenedor">
                <div class="div1"><img style="width:85%;" src="assets/img/imawari.png"   alt="..." ></div>
                 <div class="div2"><h5 class="contenido">Canaima es un proyecto del Estado que está diseñado pensando en las necesidades y realidades del servidor(ra) público venezolano, orientado a los procesos de la Administración Pública Nacional, en apoyo a su automatización con Software Libre, siendo éste una alternativa al modelo dependiente tradicional y haciendo frente a un mercado cercado por las grandes corporaciones en su mayoría transnacionales.</h5>
-                 <input type="submit"  clas="boto" value="Sitio web <img src='assets/img/flecha.png'>">
+               <button class="boto">Visite su sitio web</button>
               </div>
          </div>
          
