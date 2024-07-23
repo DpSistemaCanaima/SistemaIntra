dc4a92ef5f526de6b168ddb65561d4572d7f7744
DpSistemaCanaima <dpdesarrollocanaima@gmail.com>
2024-07-23 13:08:26 -0400

    Recreación de plantillas para la intranet

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php 
+    include "content/inc/header.php";
+?>
+<body class="bg-gray-200">
+  <main class="main-content  mt-0">
+    <div class="page-header align-items-start min-vh-100" style="background-image: url('assets/img/noticias/indus2.jpg');">
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 col-12 mx-auto">
+            <div class="card z-index-0 fadeIn3 fadeInBottom">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Ingresar</h4>
+                </div>
+              </div>
+              <div class="card-body">
+                <form role="form" class="text-start">
+                  <div class="input-group input-group-outline my-3">
+                    <label class="form-label">Usuario</label>
+                    <input type="email" class="form-control">
+                  </div>
+                  <div class="input-group input-group-outline mb-3">
+                    <label class="form-label">Contraseña</label>
+                    <input type="password" class="form-control">
+                  </div>
+                  <div class="text-center">
+                    <button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Iniciar Sesión</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="footer position-absolute bottom-2 py-2 w-100">
+        <div class="container">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-12 col-md-6 my-auto">
+              <div class="copyright text-center text-sm text-white text-lg-start">
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>,
+                Desarrollado con <i class="fa fa-heart" aria-hidden="true"></i> por
+                <a href="https://www.industriacanaima.gob.ve/" class="font-weight-bold text-white" target="_blank">Industria Canaima C.A</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </main>
+  <?php
+    include "content/inc/script.php";
+  ?>
+</body>
+
+</html>
---
+++ plantilla.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="es">
+ <?php
+  include "content/inc/header.php";
+ ?>
+  <body class="g-sidenav-show  bg-gray-100">
+      <?php 
+        include "content/inc/sidebar.php";
+      ?>
+      <main class="main-content border-radius-lg ">
+        <?php 
+            include "content/inc/navbar.php";
+        ?>
+<!-- End Navbar -->
+  <div class="container-fluid py-4"> 
+    <div class="row">
+      <div class="col-12">
+        <div id="globe" class="position-absolute end-0 top-10 mt-sm-3 mt-7 me-lg-7">
+          <canvas width="700" height="600" class="w-lg-100 h-lg-100 w-75 h-75 me-lg-0 me-n10 mt-lg-5"></canvas>
+        </div>
+      </div>
+    </div>
+    <!--De aqui en adelante agregar el contenido que va para cada vista-->
+
+    <div class="col-12 mt-4">
+              <div class="mb-5 ps-3">
+                <h6 class="mb-1">Noticias</h6>
+                <p class="text-sm">Conoce los ultimos acontecimientos de Industria Canaima C.A</p>
+              </div>
+              <div class="row">
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="card-header p-0 mt-n4 mx-3">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="assets/img/noticias/indus2.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body p-3">
+                      <p class="mb-0 text-sm">Noticia #2</p>
+                      <a href="javascript:;">
+                        <h5>
+                          Modern
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        As Uber works through a huge amount of internal management turmoil.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <button type="button" class="btn btn-outline-primary btn-sm mb-0">Ver Mas</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="card-header p-0 mt-n4 mx-3">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="assets/img/noticias/robotica_1.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+                      </a>
+                    </div>
+                    <div class="card-body p-3">
+                      <p class="mb-0 text-sm">Noticia #1</p>
+                      <a href="javascript:;">
+                        <h5>
+                          Scandinavian
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        Music is something that every person has his or her own specific opinion about.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <button type="button" class="btn btn-outline-primary btn-sm mb-0">Ver Más</button>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="card-header p-0 mt-n4 mx-3">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="assets/img/noticias/robotica_2.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body p-3">
+                      <p class="mb-0 text-sm">Noticia #3</p>
+                      <a href="javascript:;">
+                        <h5>
+                          Minimalist
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        Different people have different taste, and various types of music.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <button type="button" class="btn btn-outline-primary btn-sm mb-0">Ver mas</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="card-header p-0 mt-n4 mx-3">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="assets/img/noticias/sistema.png" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body p-3">
+                      <p class="mb-0 text-sm">Noticia #4</p>
+                      <a href="javascript:;">
+                        <h5>
+                          Gothic
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        Why would anyone pick blue over pink? Pink is obviously a better color.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <button type="button" class="btn btn-outline-primary btn-sm mb-0">Ver Más</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+    <!--Pie de pagina de la plantilla.-->
+    <footer class="footer py-4  ">
+      <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="copyright text-center text-sm text-muted text-lg-start">
+              © <script>
+                document.write(new Date().getFullYear())
+              </script>,
+              Desarrollado con <i class="fa fa-heart"></i> por
+              <a href="https://www.industriacanaima.gob.ve/" class="font-weight-bold" target="_blank">Industria Canaima C.A</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="https://www.industriacanaima.gob.ve/" class="nav-link text-muted" target="_blank">Industria Canaima C.A</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</main>
+<?php
+  include "content/inc/script.php";
+?>
+  </body>
+</html>
