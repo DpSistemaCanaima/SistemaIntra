9340688029d24fd6fc7077b30a6042c04ea89994
DpSistemaCanaima <dpdesarrollocanaima@gmail.com>
2024-08-08 11:19:28 -0400

    Conexion

--- config/cone.php
+++ config/cone.php
@@ -2,7 +2,7 @@
 
 
  // $conn = mysqli_connect('bd.industriacanaima.gob.ve','intranet','123456','Intranet');
-  $conn = mysqli_connect('localhost','root','','intranet');
+  $conn = mysqli_connect('localhost','root','123456','intranet');
 
 
   
